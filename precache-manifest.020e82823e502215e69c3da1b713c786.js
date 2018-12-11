0e6987e8116ae97c307cd7a055ad31531b9755f9
cary205 <cary205@hotmail.com>
2018-12-11 06:28:29 +0000

    Updates

---
+++ precache-manifest.020e82823e502215e69c3da1b713c786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b6bb79f6fb3535e8fd3",
+    "url": "/react-hello/static/js/runtime~main.3b6bb79f.js"
+  },
+  {
+    "revision": "cba8af20dd9e32de3642",
+    "url": "/react-hello/static/js/main.cba8af20.chunk.js"
+  },
+  {
+    "revision": "60d8e22519fed99c1db5",
+    "url": "/react-hello/static/js/1.60d8e225.chunk.js"
+  },
+  {
+    "revision": "cba8af20dd9e32de3642",
+    "url": "/react-hello/static/css/main.ee249432.chunk.css"
+  },
+  {
+    "revision": "786384cae1dcdfdde7cffa3724b49c9d",
+    "url": "/react-hello/index.html"
+  }
+];
