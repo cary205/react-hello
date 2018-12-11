4f3ea7706cec503d5a3218c93ecc47fa67f58690
cary205 <cary205@hotmail.com>
2018-12-11 06:32:52 +0000

    add homepage sample

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloMessage from './HelloMessage';
+import Timer from './Timer';
+import TodoApp from './TodoApp';
+import MarkdownEditor from './MarkdownEditor';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HelloMessage name="Taylor" />, document.getElementById('helloMessage'));
+ReactDOM.render(<Timer />, document.getElementById('timer'));
+ReactDOM.render(<TodoApp />, document.getElementById('todoApp'));
+ReactDOM.render(<MarkdownEditor />, document.getElementById('markdownEditor'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
