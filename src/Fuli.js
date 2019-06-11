3924268e757e236e54e3294ea27c9f16356e17aa
cary205 <cary205@hotmail.com>
2019-06-11 22:17:51 +0800

    update Fuli

--- src/Fuli.js
+++ src/Fuli.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+//To use the HTTP server and client one must require('http').
 const http = require('http');
 
 class Fuli extends Component {
@@ -9,12 +10,23 @@ class Fuli extends Component {
   
   componentDidMount() {
     fetch("https://gank.io/api/random/data/%E7%A6%8F%E5%88%A9/20").then(res => res.json()).then(data => {
+      console.log(JSON.stringify(data));
       this.setState({
         data: data.results
       });
+    });
       
-      http.get('https://gank.io/api/random/data/%E7%A6%8F%E5%88%A9/2', (res) => {
-        console.log("http res: " + JSON.stringify(res));
+    http.get('https://gank.io/api/random/data/%E7%A6%8F%E5%88%A9/2', (res) => {
+      console.log("http res: " + JSON.stringify(res));
+
+      let data = '';
+      // A chunk of data has been recieved.
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      // The whole response has been received. Print out the result.
+      res.on('end', () => {
+        console.log(data);
       });
     });
     
